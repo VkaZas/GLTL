526b37ecd36039d018041ca355b6dcf65ffea468
vkazas <vkazaswhite@gmail.com>
2017-09-18 15:27:38 -0400

    [chore]: Add gulp, babelify and browserify

---
+++ index.js
@@ -0,0 +1,11 @@
+let LTL = require('algorithm/LTL');
+let $ = require('jquery');
+let GridWorldPainter = require('gridworld-painter');
+
+let painter;
+
+$(document).ready(() => {
+    painter = new GridWorldPainter();
+
+});
+
---
+++ src/app.js
@@ -0,0 +1,3 @@
+let a = 5;
+let b = 20123;
+let c = 'hahaha';
